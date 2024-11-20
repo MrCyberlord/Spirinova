6573cbe4cc61635fdf3f4f8a5c9b24d5357563cd
MrCyberlord <amanjain2691@gmail.com>
2024-11-20 13:46:50 +0530

    possible error solution1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/Layout/Header';
+import Header from './components/layout/Header';
 import Footer from './components/Layout/Footer';
 import ProductList from './components/Product/ProductList';
 import FilterSidebar from './components/Filter/FilterSidebar';
